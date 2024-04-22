e9997823771b3b108052e5840ad7d64c23a813d2
Johan Nordholm <johan.nordholm@essity.com>
2024-04-23 00:26:52 +0200

    Fix failing tests & refactor IsTollFreeDate

--- src/CongestionTax.Api/Domain/TollCalculator.cs
+++ src/CongestionTax.Api/Domain/TollCalculator.cs
@@ -1,6 +1,7 @@
 ﻿namespace CongestionTax.Api.Domain;
 
 using System;
+using System.Runtime.CompilerServices;
 
 public class TollCalculator
 {
@@ -20,6 +21,24 @@ public class TollCalculator
         VehicleType.Diplomat,
         VehicleType.Foreign,
         VehicleType.Military
+    };
+
+    private readonly HashSet<DateTime> _publicHolidays = new HashSet<DateTime> 
+    {
+        new DateTime(2013, 1, 1),
+        new DateTime(2013, 1, 6),
+        new DateTime(2013, 3, 29),
+        new DateTime(2013, 3, 30),
+        new DateTime(2013, 4, 1),
+        new DateTime(2013, 5, 1),
+        new DateTime(2013, 5, 9),
+        new DateTime(2013, 5, 19),
+        new DateTime(2013, 6, 6),
+        new DateTime(2013, 6, 22),
+        new DateTime(2013, 11, 2),
+        new DateTime(2013, 12, 25),
+        new DateTime(2013, 12, 26),
+        new DateTime(2013, 12, 31)
     };
 
     public int GetTollFee(Vehicle vehicle, DateTime[] dates)
@@ -70,9 +89,10 @@ public class TollCalculator
         else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
         else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
         else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
+        else if (hour >= 8 && hour <= 14 && minute <= 59) return 8;
         else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
+        else if (hour == 15 && minute >= 30) return 18;
+        else if (hour == 16 && minute <= 59) return 18;
         else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
         else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
         else return 0;
@@ -80,26 +100,26 @@ public class TollCalculator
 
     public bool IsTollFreeDate(DateTime date)
     {
-        int year = date.Year;
-        int month = date.Month;
-        int day = date.Day;
-
-        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
-
-        if (year == 2013)
+        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
         {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
+            return true;
         }
+
+        if (date.Month == 7)
+        {
+            return true;
+        }
+
+        if (_publicHolidays.Select(o => o.Date).Contains(date.Date) == true)
+        {
+            return true;
+        }
+
+        if (_publicHolidays.Select(o => o.Date.AddDays(-1)).Contains(date.Date) == true)
+        {
+            return true;
+        }
+
         return false;
     }
 }
