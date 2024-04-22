056753789fc77443c72b000a8902cc86e3781f64
Johan Nordholm <johan.nordholm@essity.com>
2024-04-22 22:38:46 +0200

    Add Api project and make runnable

---
+++ src/CongestionTax.Api/Program.cs
@@ -0,0 +1,32 @@
+using TollFeeCalculator;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/calculatetax", (DateTime[] timestamps) => {
+    var calculator = new TollCalculator();
+    var car = new Car();
+
+    var result = calculator.GetTollFee(car, timestamps);
+
+    return result;
+})
+.WithName("CalculateCongestionTax")
+.WithOpenApi();
+
+app.Run();
